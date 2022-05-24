ba059229413d53d95dd4d2037fb35342eb52d46d
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-05-24 11:12:25 +0200

    Move handlers to controllers to make it clean and more testable

--- src/hook.ts
+++ src/hook.ts
@@ -2,8 +2,9 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import 'dotenv/config'
 import WhatsApp from './heyoo'
-import ProcessPayload from './heyoo/processNotificationPayload'
-import { NotificationPayload } from './types/event'
+import NotificationController from './controllers/notificationController'
+import VerificationController from './controllers/verificationController'
+
 
 
 const app = express()
@@ -14,58 +15,14 @@ const port = process.env.LISTEN_PORT
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
-let messenger = new WhatsApp(process.env.TOKEN, "101404072591160")
-let VERIFY_TOKEN =  "30cca545-3838-48b2-80a7-9e43b1ae8ce4"
+// let messenger = new WhatsApp(process.env.TOKEN, "101404072591160")
 
 
-app.get('/', (req: Request, res:Response) => {
-    console.log("Request Data: ", req.query)
+// Routes
+app.get('/', VerificationController.handleVerification)
+app.post("/", NotificationController.handleNotifications)
 
-    // Handle Verification
-    if (req.query["hub.verify_token"]){
-        let hub:any = req.query
-        console.log("Hub: ", hub)
-        let verify_token = req.query["hub.verify_token"]
-
-        if (verify_token == VERIFY_TOKEN){
-          return res.send(req.query["hub.challenge"])
-        }
-        return res.send("Invalid verification token")
-    }
-
-    return res.send("This is a default response")
-})
-
-app.post("/", (req: Request, res: Response) => {
-    console.log("Request Data: ", req.body)
-
-
-    // Handle other notifications
-    let data: NotificationPayload = req.body
-    let ppayload = new ProcessPayload(data)
-
-    if (ppayload.type == "messages"){
-        let messages = ppayload.get_messages()
-
-        for (const message of messages){
-            if (message.type == "text"){
-                console.log("Message Data: ", message)
-
-                // Send Message Here
-                // messenger.send_message("Yeah I saw it", message.from)
-                //         .then(console.log)
-                //         .catch(console.log)
-             
-
-            }
-        }
-    } 
-
-    console.log("Payload Type: ", ppayload.type)
-    console.log("Payload Data: ", ppayload.data.entry[0].changes[0].value)
-    
-})
-
+// Listen on service
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
